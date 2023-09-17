47faa06769ee75964e3b442f61f741580f98c1a8
ArturoMP19 <a.mendoza.p552.edu.utc.mx>
2023-09-16 18:30:15 -0600

    Mi primer hola mundo

---
+++ 01Ejemplo/HolaMundo.java
@@ -0,0 +1,36 @@
+/* java maneja clases, las cuales nos sirven para poder diferenciar objetos, cada clase
+ puede poseer uno o varios objetos y tipos de datos a convivir dentro de la clase
+ 
+ la clase debe de llamarse igual que el nombre del archivo
+ */
+
+public class HolaMundo {
+
+
+    //java es un lenguaje intermedio 
+    //Tiene un lenguaje natural solo en ingles
+    // maquina virual es la que se encarga de poder transformar el lenguaje en instrucciones para la
+    //maquina jdk son los elemnetos por la compilacion, interpretacion y transformaciones del codigo 
+    // es un paradigma a objetos 
+    //toda clase debe de tener un metodo principal 
+
+    public static void main(String []args){
+
+    // public: es un metodo publico significa que cualquiera tiene acceso a dicho metodo
+    //un metodo es un comportamiento de la clase
+    //static/estatico, significa que no tiene cambios el metodo, que a su momento de ejecutarlo 
+    //no va a presentar cambios en sus componentes, parametros, elementos,etc
+    //void que no devuelve nada es un metodo vacio , que no es necesario manipular los parametros 
+    System.out.println("Hola mundo, ya aprendioeron su primer codigo");
+    //System en la clase que manda a llamar a la libreria que se encarga de los comportamientos del sistema 
+    //ou es el objeto de salida estandar de la computadora osea la pantalla 
+    //println es le metodo , para que el sistema pueda imprimir en pantalla 
+    //tercera regla , toda linea de codgido tiene un fin marcado con un ;
+    
+    //javac nos sirve paraa compilar el archivo
+    //java es para mandar a llamar el archivo coberbyt
+    }
+
+}
+
+
